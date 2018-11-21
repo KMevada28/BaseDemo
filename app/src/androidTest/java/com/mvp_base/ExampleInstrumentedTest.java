99991520d2a10e32da61ae63f05a8bb52468ac24
KMevada28 <kundankumar.mevada@nihilent.com>
2018-11-21 15:03:37 +0530

    package renamed

--- app/src/androidTest/java/com/mvp_base/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mvp_base/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.daggerdemo", appContext.getPackageName());
+        assertEquals("com.mvp_base", appContext.getPackageName());
     }
 }
